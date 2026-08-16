8129c196956c0ad72058a8908e49c25050b81fe3
Gerardo <gerardohuerta1502@gmail.com>
2026-08-16 16:25:41 -0600

    feat: Serivicos para el rover

---
+++ internal/service/rover_service.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/GerardoHuHer/go_crud/internal/domain"
+	"github.com/GerardoHuHer/go_crud/internal/repository"
+)
+
+var ErrIdTaken = errors.New("Id already taken")
+
+type RoverService struct {
+	repo repository.RoverRepository
+}
+
+func NewRoverService(repo repository.RoverRepository) *RoverService {
+	return &RoverService{repo: repo}
+}
+
+func (r *RoverService) CreateRover(ctx context.Context, pos_x, pos_y int) (*domain.Rover, error) {
+	rover := &domain.Rover{Pos_x: pos_x, Pos_y: pos_y}
+	if err := r.repo.Create(ctx, rover); err != nil {
+		return nil, err
+	}
+	return rover, nil
+}
+
+func (r *RoverService) GetRoverById(ctx context.Context, id uint) (*domain.Rover, error) {
+	return r.repo.FindById(ctx, id)
+}
+
+func (r *RoverService) UpdateRoverById(ctx context.Context, id uint, pos_x, pos_y int) (*domain.Rover, error) {
+	rover, err := r.repo.FindById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	rover.Pos_x = pos_x
+	rover.Pos_y = pos_y
+
+	if err := r.repo.Update(ctx, rover); err != nil {
+		return nil, err
+	}
+	return rover, nil
+}
+
+func (r *RoverService) DeleteRoverById(ctx context.Context, id uint) error {
+	return r.repo.Delete(ctx, id)
+}
